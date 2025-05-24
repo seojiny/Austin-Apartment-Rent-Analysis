3930a9b63a14f4c629d6a40d262fb65597d44199
Seojin Yoon <syoon0320@gmail.com>
2025-05-24 17:43:24 -0500

    Update files

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# Austin-Apartment-Rent-Analysis
-Austin Apartment Rent analysis using SQL and Tableau
+# Austin Apartment Rent Analysis (2022 - 2024)
+Austin Apartment Rent Analysis of 2022 - 2024 using SQL and Tableau. 
+* Interactive Tableau dashboard found [here](https://public.tableau.com/app/profile/seojin.yoon/viz/AustinApartmentRentAnalysis2022-2024/AustinApartmentRentalTrendsDemandPatterns2022-2024?publish=yes)
+
+![alt text](austin-rent-dashboard.png)
+
+## Metrics & Dimensions
+* **Location:** Austin,TX and United States
+* **Vacancy Index:** Percentage(%) of available apartment units that are vacant or unoccupied
+* **Days On Market:** Number of days an apartment unit is on the market
+* **Median Rent:** Middle value of apartment unit rent price($) of overall bedroom sizes
+
+## Datasets
+* Apartment Rental Market datasets from Jan 2017 to Apr 2025 from [Apartment List](https://www.apartmentlist.com/research/category/data-rent-estimates)
+
+## Objective
+* Analyze apartment market data to demand patterns for renting an apartment in Austin.
+
+### Data Questions
+* When is the best time in the year for finding an apartment in Austin?
+* Is Austin's apartment market becoming more of a heating or cooling market? 
+* How have average rent prices in Austin changed in recent years? How does this compare to the U.S.?
+
+## Summary of Insights
+
+* The average price to rent an apartment in Austin in 2024 was $1,436.39. Apartment prices are lowest in December and highest from June through August.
+
+* From 2022 - 2024, the average vacancy index was 7.69% and average days on market was 31.14 days, indicating Austin’s apartment market is likely entering a period of cooling. Throughout the year, demand is gradually declines from September to December. Demand is picks up again starting in January and becomes strong throughout the sumemr months of May to August, where rent prices grow significantly higher than each previous month.
+    * Months from September to December are typically he best time to find an apartment in Austin, as it's enters a period of a cooling market. There are more apartment listing available at this time, they tend to stay on the market longer, and there's a slower rent growth month-over-month.
+
+* Median Rent prices have been gradually decreasing in Austin and in the U.S. from 2022 - 2024. This decline aligns with Austin's direction towards a cooling market, likely due to increased apartment construction with shifts in demand. Austin's median rent was 7.17% higher than the U.S. national average in 2024.
--- apartment_analysis.sql
+++ apartment_analysis.sql
@@ -22,27 +22,26 @@ FROM apartment_rent_growth_yoy_staging;
 
 
 -- **** ANALYSIS OF AUSTIN **** --
-	# Monthly averages based over 1-3 years to capture seasonal trends. 
-	# Year to year comparisons based over 3-5 years to capture yearly trends.
+	# Monthly & Yearly averages based over 2022 - 2024 years to capture seasonal trends. 
 
 -- 1. Austin > Average Vacancy Index by Month in past 2 years
 SELECT 
-  MONTHNAME(`date`) AS month_name,
-  MONTH(`date`) AS month_num,
-  ROUND(AVG(vacancy_index) * 100, 2) AS avg_vacancy_index
-FROM apartment_vacancy_staging
-WHERE location_name = 'Austin, TX'
-  AND `date` >= DATE_SUB(CURRENT_DATE, INTERVAL 2 YEAR)
-GROUP BY month_name, month_num
-ORDER BY month_num; #ORDER BY avg_vacancy_index DESC;
-
--- 2. Austin > Average Vacancy Index by Year in past 4 years
-SELECT 
-	YEAR(`date`) AS year, 
+    MONTHNAME(`date`) AS month_name,
+    MONTH(`date`) AS month_num,
     ROUND(AVG(vacancy_index) * 100, 2) AS avg_vacancy_index
 FROM apartment_vacancy_staging
 WHERE location_name = 'Austin, TX'
-	AND `date` >= DATE_SUB(CURRENT_DATE, INTERVAL 4 YEAR)
+    AND `date` BETWEEN '2022-01-1' AND '2024-12-01'
+GROUP BY month_name, month_num
+ORDER BY month_num; #ORDER BY avg_vacancy_index DESC;
+
+-- 2. Austin > Average Vacancy Index by Year in past 2 years
+SELECT 
+    YEAR(`date`) AS year, 
+    ROUND(AVG(vacancy_index) * 100, 2) AS avg_vacancy_index
+FROM apartment_vacancy_staging
+WHERE location_name = 'Austin, TX'
+	AND `date` BETWEEN '2022-01-1' AND '2024-12-01'
 GROUP BY year
 ORDER BY year;
 
@@ -53,50 +52,50 @@ SELECT
     ROUND(AVG(days_on_market), 2) AS avg_days
 FROM apartment_tom_staging
 WHERE location_name = 'Austin, TX' 
-	AND `date` >= DATE_SUB(CURRENT_DATE, INTERVAL 2 YEAR)
+    AND `date` BETWEEN '2022-01-1' AND '2024-12-01'
 GROUP BY month_name, month_num
 ORDER BY month_num; #ORDER BY avg_days DESC;
 
--- 4. Austin > Average Time On Market by Year in past 4 years
+-- 4. Austin > Average Time On Market by Year in past 2 years
 SELECT
-	YEAR(`date`) AS year, 
+    YEAR(`date`) AS year, 
     ROUND(AVG(days_on_market), 2) AS avg_days
 FROM apartment_tom_staging
 WHERE location_name = 'Austin, TX' 
-	AND `date` >= DATE_SUB(CURRENT_DATE, INTERVAL 4 YEAR)
+	AND `date` BETWEEN '2022-01-1' AND '2024-12-01'
 GROUP BY year
 ORDER BY year;
 
 -- 5. Austin > Average Rent Estimates by Month in past 2 years
 SELECT 
-	MONTHNAME(`date`) AS month_name, 
+    MONTHNAME(`date`) AS month_name, 
     MONTH(`date`) AS month_num,
     ROUND(AVG(median_rent), 2) AS avg_monthly_median_rent
 FROM apartment_rent_estimates_staging
 WHERE location_name = 'Austin, TX' 
-	AND bed_size = 'overall'
-	AND `date` >= DATE_SUB(CURRENT_DATE, INTERVAL 2 YEAR)
+    AND bed_size = 'overall'
+    AND `date` BETWEEN '2022-01-1' AND '2024-12-01'
 GROUP BY month_name, month_num
 ORDER BY month_num; #ORDER BY avg_monthly_median_rent DESC;
 
--- 6. Austin > Average Rent Estimates by Year in past 4 years
+-- 6. Austin > Average Rent Estimates by Year in past 2 years
 SELECT
-	YEAR(`date`) AS year, 
+    YEAR(`date`) AS year, 
     ROUND(AVG(median_rent), 2) AS avg_monthly_median_rent
 FROM apartment_rent_estimates_staging
 WHERE location_name = 'Austin, TX' 
-	AND `date` >= DATE_SUB(CURRENT_DATE, INTERVAL 4 YEAR)
+    AND `date` BETWEEN '2022-01-1' AND '2024-12-01'
 GROUP BY year
 ORDER BY year;
 
 -- 7. Austin > Average Yearly Rent Growth between Months in past 2 years
 SELECT 
-	MONTHNAME(`date`) AS month_name, 
+    MONTHNAME(`date`) AS month_name, 
     MONTH(`date`) AS month_num, 
     ROUND(AVG(growth_rate) * 100, 2) AS growth_rate
 FROM apartment_rent_growth_yoy_staging
 WHERE location_name = 'Austin, TX' 
-	AND `date` >= DATE_SUB(CURRENT_DATE, INTERVAL 2 YEAR)
+    AND `date` BETWEEN '2022-01-1' AND '2024-12-01'
 GROUP BY month_name, month_num
 ORDER BY month_num;
 
@@ -105,101 +104,100 @@ ORDER BY month_num;
 	-- Cooling Market: *High Vacancy, High Time on Market, Low Rent Growth -- Demand is slower
     -- Heating Market: Low Vacancy, Low Time on Market, High Rent Growth -- Demand is strong
 SELECT
-	MONTHNAME(v.`date`) AS month_name,
+    MONTHNAME(v.`date`) AS month_name,
     MONTH(v.`date`) AS month_num,
     ROUND(AVG(v.vacancy_index) * 100, 2) AS vacancy_index,
-    ROUND(AVG(r.median_rent), 2) AS median_rent,
-    ROUND(AVG(t.days_on_market), 2) AS days_on_market
+    ROUND(AVG(t.days_on_market), 2) AS days_on_market,
+    ROUND(AVG(r.median_rent), 2) AS median_rent
 FROM apartment_vacancy_staging v
 INNER JOIN apartment_tom_staging t
-	ON v.location_name = t.location_name
+    ON v.location_name = t.location_name
     AND v.`date` = t.`date`
 INNER JOIN apartment_rent_estimates_staging r
-	ON v.location_name = r.location_name
+    ON v.location_name = r.location_name
     AND v.`date` = r.`date`
 WHERE v.location_name = 'Austin, TX'
-	AND v.`date` >= DATE_SUB(CURRENT_DATE, INTERVAL 2 YEAR)
+    AND v.`date` BETWEEN '2022-01-1' AND '2024-12-01'
 GROUP BY month_num, month_name
 ORDER BY
-	vacancy_index DESC,
-    days_on_market DESC,
-    median_rent ASC;
+    vacancy_index DESC,
+    days_on_market DESC;
     
 
 -- **** ANALYSIS OF UNITED STATES & MAJOR CITIES IN TEXAS **** -- 
 
 -- 1. United States > Average Vacancy Index by Month in past 2 years
 SELECT
-	MONTHNAME(`date`) AS month_name,
+    MONTHNAME(`date`) AS month_name,
     MONTH(`date`) AS month_num,
     ROUND(AVG(vacancy_index) * 100, 2) AS avg_vacancy_index
 FROM apartment_vacancy_staging
 WHERE location_name = 'United States'
-	AND `date` >= DATE_SUB(CURRENT_DATE, INTERVAL 2 YEAR)
+    AND `date` BETWEEN '2022-01-1' AND '2024-12-01'
 GROUP BY month_name, month_num
 ORDER BY month_num; #ORDER BY avg_vacancy_index DESC;
 
--- 2. United States > Average Vacancy Index by Year in past 4 years
+-- 2. United States > Average Vacancy Index by Year in past 2 years
 SELECT
-	YEAR(`date`) AS year,
-	ROUND(AVG(vacancy_index) * 100, 2) AS avg_vacancy_index
+    YEAR(`date`) AS year,
+    ROUND(AVG(vacancy_index) * 100, 2) AS avg_vacancy_index
 FROM apartment_vacancy_staging
 WHERE location_name = 'United States'
-	AND `date` >= DATE_SUB(CURRENT_DATE, INTERVAL 4 YEAR)
+    AND `date` BETWEEN '2022-01-1' AND '2024-12-01'
 GROUP BY year
 ORDER BY year;
 
 -- 3. United States > Average Time On Market by Month in past 2 years
 SELECT
-	MONTHNAME(`date`) AS month_name,
+    MONTHNAME(`date`) AS month_name,
     MONTH(`date`) AS month_num,
     ROUND(AVG(days_on_market), 2) AS avg_days
 FROM apartment_tom_staging
 WHERE location_name = 'United States'
-	AND `date` >= DATE_SUB(CURRENT_DATE, INTERVAL 2 YEAR)
+    AND `date` BETWEEN '2022-01-1' AND '2024-12-01'
 GROUP BY month_name, month_num
 ORDER BY month_num; #ORDER BY avg_days DESC;
 
--- 4. United States > Average Time On Market by Year in past 4 years
+-- 4. United States > Average Time On Market by Year in past 2 years
 SELECT
-	YEAR(`date`) AS year,
+    YEAR(`date`) AS year,
     ROUND(AVG(days_on_market), 2) AS avg_days
 FROM apartment_tom_staging
 WHERE location_name = 'United States'
-	AND `date` >= DATE_SUB(CURRENT_DATE, INTERVAL 4 YEAR)
+    AND `date` BETWEEN '2022-01-1' AND '2024-12-01'
 GROUP BY year
 ORDER BY year;
 
 -- 5. United States > Average Rent Estimates by Month in past 2 years
 SELECT
-	MONTHNAME(`date`) AS month_name,
+    MONTHNAME(`date`) AS month_name,
     MONTH(`date`) AS month_num,
     ROUND(AVG(median_rent), 2) AS avg_monthly_median_rent
 FROM apartment_rent_estimates_staging
 WHERE location_name = 'United States'
-	AND bed_size = 'overall'
-    AND `date` >= DATE_SUB(CURRENT_DATE, INTERVAL 2 YEAR)
+    AND bed_size = 'overall'
+    AND `date` BETWEEN '2022-01-1' AND '2024-12-01'
 GROUP BY month_name, month_num
 ORDER BY month_num; #ORDER BY avg_monthly_median_rent DESC;
 
--- 6. United States > Average Rent Estimates by Year in past 4 years
+-- 6. United States > Average Rent Estimates by Year in past 2 years
 SELECT
-	YEAR(`date`) AS year,
+    YEAR(`date`) AS year,
     ROUND(AVG(median_rent), 2) AS avg_monthly_median_rent
 FROM apartment_rent_estimates_staging
 WHERE location_name = 'United States'
-	AND `date` >= DATE_SUB(CURRENT_DATE, INTERVAL 4 YEAR)
+    AND `date` BETWEEN '2022-01-1' AND '2024-12-01'
 GROUP BY year
 ORDER BY year;
 
 -- 7. United States > Average Yearly Rent Growth between Months in past 2 years
 SELECT
-	MONTHNAME(`date`) AS month_name,
+    MONTHNAME(`date`) AS month_name,
     MONTH(`date`) AS month_num,
     ROUND(AVG(growth_rate) * 100, 2) AS growth_rate
 FROM apartment_rent_growth_yoy_staging
 WHERE location_name = 'United States'
-	AND `date` >= DATE_SUB(CURRENT_DATE, INTERVAL 2 YEAR)
+    AND `date` BETWEEN '2022-01-1' AND '2024-12-01'
 GROUP BY month_name, month_num
 ORDER BY month_num;
 
@@ -207,55 +205,54 @@ ORDER BY month_num;
 	-- Cooling Market: *High Vacancy, High Time on Market, Low Rent Growth -- Demand is slower
     -- Heating Market: Low Vacancy, Low Time on Market, High Rent Growth -- Demand is strong
 SELECT
-	MONTHNAME(v.`date`) AS month_name,
+    MONTHNAME(v.`date`) AS month_name,
     MONTH(v.`date`) AS month_num,
     ROUND(AVG(v.vacancy_index) * 100, 2) AS vacancy_index,
     ROUND(AVG(r.median_rent), 2) AS median_rent,
     ROUND(AVG(t.days_on_market), 2) AS days_on_market
 FROM apartment_vacancy_staging v
 INNER JOIN apartment_tom_staging t
-	ON v.location_name = t.location_name
+    ON v.location_name = t.location_name
     AND v.`date` = t.`date`
 INNER JOIN apartment_rent_estimates_staging r
-	ON v.location_name = r.location_name
+    ON v.location_name = r.location_name
     AND v.`date` = r.`date`
 WHERE v.location_name = 'United States'
-	AND v.`date` >= DATE_SUB(CURRENT_DATE, INTERVAL 2 YEAR)
+    AND v.`date` BETWEEN '2022-01-1' AND '2024-12-01'
 GROUP BY month_num, month_name
 ORDER BY
-	vacancy_index DESC,
-    days_on_market DESC,
-    median_rent ASC;
+    vacancy_index DESC,
+    days_on_market DESC;
 
 -- 9. United States > Cities by Highest Vacancy Index in the past 2 years
 SELECT
-	location_name,
+    location_name,
     ROUND(MAX(vacancy_index) * 100, 2) AS vacancy_index
 FROM apartment_vacancy_staging
 WHERE location_type = 'city'
-	AND `date` >= DATE_SUB(CURRENT_DATE, INTERVAL 2 YEAR)
+    AND `date` BETWEEN '2022-01-1' AND '2024-12-01'
 GROUP BY location_name
 ORDER BY vacancy_index DESC
 LIMIT 10;
 
 -- 10. United States > Average Vacancy Index of Major Cities in Texas by population in past 2 years
 SELECT location_name,
-	ROUND(AVG(vacancy_index) * 100, 2) AS vacancy_index
+    ROUND(AVG(vacancy_index) * 100, 2) AS vacancy_index
 FROM apartment_vacancy_staging
 WHERE location_type = 'city'
-	AND state = 'Texas'
-    AND `date` >= DATE_SUB(CURRENT_DATE, INTERVAL 2 YEAR)
+    AND state = 'Texas'
+    AND `date` BETWEEN '2022-01-1' AND '2024-12-01'
 GROUP BY location_name, population
 ORDER BY population DESC
 LIMIT 5;
 
 -- 11. United States > Average Rent Estimates of Major Cities in Texas by population in past 2 years
 SELECT location_name,
-	ROUND(AVG(median_rent), 2) AS median_rent
+    ROUND(AVG(median_rent), 2) AS median_rent
 FROM apartment_rent_estimates_staging
 WHERE location_type = 'city'
-	AND state = 'Texas'
-    AND `date` >= DATE_SUB(CURRENT_DATE, INTERVAL 2 YEAR)
+    AND state = 'Texas'
+    AND `date` BETWEEN '2022-01-1' AND '2024-12-01'
 GROUP BY location_name, population
 ORDER BY population DESC
 LIMIT 5;
